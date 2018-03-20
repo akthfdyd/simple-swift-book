4d3a13ddd40cb782452687ccbc28030edb7d4305
akthfdyd <akthfdyd@gmail.com>
2018-03-20 20:40:12 +0900

    Update main.swift

--- main.swift
+++ main.swift
@@ -8,9 +8,14 @@ print("\n\n#lesson 1 - Constant, Variable \n")
 // main()함수는 없습니다
 // C와 같은 file dependency는 없습니다
 // 대신 module 을 가져옵니다
+
 // 상수(변경불가)는 let으로, 변수는 var로 선언 합니다
 let CONSTANT_INT : Int = 10
 var variableInt : Int = 10
+
+// 이름은 영문이 아니어도 유니코드로 작성 가능합니다. 그러나 자제해야겠지요
+var 변수 = "var"
+print(변수)
 
 // value type은 자동으로도 찾아 줍니다
 let CONSTANT_STRING = "const"
@@ -30,13 +35,14 @@ var numberPrintValue = 5
 var stringPrintValue = "입니다"
 print("출력하려는 값은 \(numberPrintValue) \(stringPrintValue)")
 
-// swift의 기본형은 빈값(nil)을 담을 수 없습니다.
+// swift의 기본자료형은 빈값(nil)을 담을 수 없습니다.
 // 빈값을 담을 수 있는 Optional type이라는 것이 별도로 존재합니다
 // 자료형 뒤에 ?를 붙여 선언합니다
 var optValue : String? = nil
 optValue = "optional type"
-print(optValue) // 직접사용이 불가. 컴파일중 warning이 뜹니다
+//print(optValue) // 직접사용이 불가! 컴파일중 warning이 뜹니다
 print(optValue!) // 이렇게 강제로 unwraping()합니다
+
 // Optional 타입 선언시 자동 unwraping이 되도록 하려면 !를 이용합니다
 var optAutoChange : String! = nil
 optAutoChange = "auto casting optional type"
@@ -278,7 +284,50 @@ extension BrunchCafe {
 
 print("\n\n#lesson - Extension with Protocol")
 
-// initializer가 없다는 전제 하에, 익스텐션이 프로토콜을 구현할 수 있습니다.
+// initializer가 없다는 전제 하에, 익스텐션이 프로토콜을 확장구현할 수 있습니다.
+protocol Pasta {
+//    func boilWater(){ // 에러! 프로토콜에서는 함수의 내용을 구현할 수 없습니다.
+//        print("boiling")
+//    }
+}
+
+// 하지만 익스텐션에서는 함수의 구현이 가능합니다.
+extension Pasta {
+    func boilWater(){
+        print("물 끓이는 중")
+    }
+}
+
+// 구조체로 가져올 수 있겠지요.
+struct Breakfast: Pasta{}
+var myMeal = Breakfast()
+myMeal.boilWater()
+
+// 하나 더 만들어 봅니다.
+protocol Pizza {}
+extension Pizza {
+    func chopVegetables(){
+        print("야채 써는 중")
+    }
+}
+
+// 클래스를 구현할 때 다중 상속의 효과를 낼 수 있습니다.
+class ItalianBrunch: Pasta, Pizza {
+    func boilWater(){ // 프로토콜 익스텐션에서 먼저 구현했던 함수를 재정의할 수도 있습니다.
+        print("다시 물 끓이는 중")
+    }
+}
+
+// 실행해 봅시다.
+var myLunch = ItalianBrunch()
+myLunch.chopVegetables()
+myLunch.boilWater()
+
+// 다른 클래스 간 동일한 코드의 중복을 방지할 수 있습니다.
+class ItalianDinner: Pasta, Pizza {}
+var myDinner = ItalianDinner()
+myDinner.chopVegetables()
+myDinner.boilWater()
 
 
 
@@ -287,17 +336,36 @@ print("\n\n#lesson - Extension with Protocol")
 
 
 
+print("\n\n#lesson - Error handling")
 
+let logPath = "./" // 잘못된 파일명을 입력
+var str = "savedata"
 
+//throws로 핸들링하도록 만들어진 함수는 꼭 do try catch 를 붙여서 에러핸들링을 해야 한다.
+do {
+    try str.write(toFile: logPath, atomically: true, encoding: String.Encoding.utf8)
+} catch {
+    print("에러 발생")
+}
 
+// 에러를 가져오려면 다음과 같이 합니다.
+do {
+    try str.write(toFile: logPath, atomically: true, encoding: String.Encoding.utf8)
+} catch let error{
+    print("에러 발생 -> ", error)
+}
 
+// try의 위치에 유의하기 바랍니다. 값을 반환하는 함수의 경우 대입되기 이전에 검사해 주어야 합니다.
+do{
+    let readWrongTxt = try String(contentsOfFile: logPath)
+    print(readWrongTxt)
+} catch let error{
+    print("에러 발생 -> ", error)
+}
 
-
-
-
-
-
-
+// 에러 발생시 nil이 반환되도록 강제로 optional type 캐스팅할 수 있습니다.
+let readWrongTxt = try? String(contentsOfFile: logPath)
+print(type(of: readWrongTxt))
 
 
 
